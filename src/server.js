8291639c1fc3447a340882332ed755f58bf6cf89
Ana Clara Lélis <anaa.leliis@gmail.com>
2020-06-15 17:11:34 -0300

    -Modifiquei o arquivo home -Deletei about e home -Criei arquivo data -Criei about e home -Modifiquei layout e and not-found

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 const express = require("express") //buscar o modulo express
 const server = express() //executar o express
+const nunjucks = require("nunjucks") //buscar o modulo nunjucks
+const data = require("./data")
+
+
+
 
 //CRIAR SERVIDOR
 server.listen("3000", function(){
@@ -8,5 +13,24 @@ server.listen("3000", function(){
 
 //CRIAR ROTAS
 server.get("/",function(req,res){
-  return res.send("index.html")
+  return res.render("home",{
+    itens:data
+  })
 })
+
+server.get("/about",function(res,res){
+  return res.render("about")
+})
+
+//Arquivos Estaticos
+server.use(express.static("public"))
+
+
+
+//CONFIGURAÇÃO NO NUNJUCKS
+server.set("view engine","njk")
+nunjucks.configure("src/views",{
+  express:server,
+  // autoescape: true
+})
+
