4a1e9d9791ba6ea264f9aad5c6a3ee2aaa7fa24a
Ana Clara Lélis <anaa.leliis@gmail.com>
2020-06-16 15:54:10 -0300

    "atualização no branch bacjk-end

--- public/scripsts/script.js
+++ public/scripsts/script.js
@@ -1,17 +1,37 @@
-const modalOverlay = document.querySelector('.modal-overlay'); //selecinar apenas o modal
-const cards = document.querySelectorAll('.card'); //selecionar todos os cards no documento
+// const modalOverlay = document.querySelector('.modal-overlay'); //selecinar apenas o modal
+const recipesList = document.querySelectorAll('.card'); //selecionar todos os cards no documento //array de elementos 
 
+
+//Criar um array com todos os elementos que tem a classe .card    route params
+for (let i = 0, len = recipesList.length; i < len; i++) { //renderizados  0,1
+  //id é a var 'i' dentro do for
+  //essa função está entre parentesis porque está usando a sí mesmo 
+  (function (index) { //quando clicar vai ser redirecionada para a rota 
+    recipesList[i].onclick = function () {
+      window.location.href = `/recipes/${index}`; //id corresponde a posição dos elementos que estão no arquivo data
+
+    };
+    //aqui eu digo que o var 'i' é parametro da função e dentro dela conseguimos
+    //utilizá-lo como o parâmetro  id 
+  })(i);
+}
+
+
+
+
+
+/*
 for(let card of cards){
   card.addEventListener("click", function(){
     modalOverlay.classList.add('active')
   })
-  
+
 }
 
 document.querySelector('.modal-rodape').addEventListener("click", function(){
   modalOverlay.classList.remove('active');
 })
-
+*/
 
 /*for(let card of cards){
 }Repetição*/
--- src/server.js
+++ src/server.js
@@ -7,30 +7,57 @@ const data = require("./data")
 
 
 //CRIAR SERVIDOR
-server.listen("3000", function(){
+server.listen("3000", function () {
   console.log("Serving conecter")
 })
 
 //CRIAR ROTAS
-server.get("/",function(req,res){
-  return res.render("home",{
-    itens:data
+server.get("/", function (req, res) {
+  return res.render("home", {
+    itens: data
   })
 })
 
-server.get("/about",function(res,res){
+server.get("/about", function (req, res) {
   return res.render("about")
 })
+
+server.get("/listRecipes", function (req, res) {
+  return res.render("listRecipes", {
+    itens: data
+  })
+})
+
+
+
+
+server.get("/recipes/:index", function(req,res){
+  const recipes = data
+  const recipeIndex = req.params.index //pega as informações ataves da url no caso seria o id
+
+  if(!recipes[recipeIndex]){
+    return res.render("not-found");
+  }
+
+  return res.render("recipes",{
+    recipes:recipes[recipeIndex]
+  })
+})
+
+
 
 //Arquivos Estaticos
 server.use(express.static("public"))
 
+server.use(function (req, res) {
+  res.status(404).render("not-found");
+})
 
 
 //CONFIGURAÇÃO NO NUNJUCKS
-server.set("view engine","njk")
-nunjucks.configure("src/views",{
-  express:server,
+server.set("view engine", "njk")
+nunjucks.configure("src/views", {
+  express: server,
   // autoescape: true
 })
 
